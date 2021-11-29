a152a15a92bfa349b72cd04b388ac89cd42487a9
Yunus Kinkhabwala <yunuskink@gmail.com>
2021-11-29 12:56:18 -0500

    Update distance_matrix_calculation.py
    Cleaned up the python file

--- distance_matrix_calculation.py
+++ distance_matrix_calculation.py
@@ -3,7 +3,7 @@
 # graph of Contra Costa county.
 # NEXT STEPS: Write for loop that will.
 # 1. Find all hubs that are within max_D of a given block group
-# 2. Start nested for loop inside to calculate travel time between hubs and blockgroups 
+# 2. Start nested for loop inside to calculate travel time between hubs and blockgroups
 
 # %% codecell
 import osmnx as ox
@@ -14,40 +14,30 @@ import pandas as pd
 
 place_name = "Contra Costa County, California, USA"
 max_D = 100
+####### COMMENTED OUT FOLLOWING TWO LINES FOR CHOOSING NEW GRAPH ###########
 # graph = ox.graph_from_place(place_name, network_type='drive')
 # ox.io.save_graphml(graph,"./data/"+place_name)
 graph = ox.io.load_graphml("./data/"+place_name)
 # Load in candidate sites
-# "C:\Users\yunus\github\sgc-deployment-scenarios\data\candidate_site_campuses_2021-11-17\candidate_site_campuses.csv"
 hubs = pd.read_csv(r"C:\Users\yunus\github\sgc-deployment-scenarios\data\candidate_site_campuses_2021-11-17\candidate_site_campuses.csv")
 hubs = hubs[hubs["cat_site"]!="X"]
-hubs
 hubs_gdf = gpd.GeoDataFrame(hubs[["id_site","SQFT_ROOF"]], geometry=gpd.points_from_xy(hubs.LON, hubs.LAT))
-
 bg_gdf = gpd.read_file(r"C:\Users\yunus\github\sgc-deployment-scenarios\data\bg_ca_19\blockgroup_CA_19.shp")
-bg_gdf.plot()
-bg_gdf
-
-# %% codecell
 # impute speed on all edges missing data
 graph = ox.add_edge_speeds(graph)
-
 # calculate travel time (seconds) for all edges
 graph = ox.add_edge_travel_times(graph)
-# %% codecell
-graph
+
 # %% codecell
 fig, ax = ox.plot_graph(ox.project_graph(graph))
 graph_proj = ox.project_graph(graph)
 nodes_proj, edges_proj = ox.graph_to_gdfs(graph_proj, nodes=True, edges=True)
 # %% codecell
-bg_gdf
-
-for lat,lon in zip(bg_gdf["INTPTLAT"],bg_gdf["INTPTLON"]):
+# Calculating distances between hubs and block groups
+for census_lat,census_lon in zip(bg_gdf["INTPTLAT"],bg_gdf["INTPTLON"]):
     # Find all hubs within max_D km
-    dist =
-    for
     # Find the tracts/block groups that are within
+    for hub_lat,hub_lon in zip(hubs_gdf["INTPTLAT"],hubs_gdf["INTPTLON"]):
 
 
 
@@ -58,8 +48,7 @@ for lat,lon in zip(bg_gdf["INTPTLAT"],bg_gdf["INTPTLON"]):
 
 
 
-
-
+########### OLD CODE BELOW ###########################
 
 
 #
