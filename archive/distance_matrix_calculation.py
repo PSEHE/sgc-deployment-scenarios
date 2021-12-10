93b4df0b75f3f2d89427a1b4b60f7dcf8c6e8740
Audrey Smith <audrey-smith@users.noreply.github.com>
2021-12-09 16:28:53 -0800

    Readability and drive time accuracy fixes

---
+++ archive/distance_matrix_calculation.py
@@ -0,0 +1,121 @@
+# %% markdown
+# PROGRESS: File below loads in condidate sites, block group geometries and,
+# graph of Contra Costa county.
+# NEXT STEPS: Write for loop that will.
+# 1. Find all hubs that are within max_D of a given block group
+# 2. Start nested for loop inside to calculate travel time between hubs and blockgroups
+
+# %% codecell
+import osmnx as ox
+import networkx as nx
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+import shapely
+
+place_name = "Contra Costa County, California, USA"
+max_D = 100
+
+print('x')
+####### COMMENTED OUT FOLLOWING TWO LINES ARE FOR CHOOSING NEW GRAPH ###########
+# graph = ox.graph_from_place(place_name, network_type='drive')
+# ox.io.save_graphml(graph,os.path.join(os.getcwd(),"./data/",place_name))
+graph = ox.io.load_graphml(os.path.join(os.getcwd(),"./data/",place_name))
+# Load in candidate sites
+hubs = pd.read_csv(os.path.join(os.getcwd(),"./data/candidate_site_campuses_2021-11-17/candidate_site_campuses.csv"))
+hubs = hubs[hubs["cat_site"]!="X"]
+hubs_gdf = gpd.GeoDataFrame(hubs[["id_site","SQFT_ROOF","LON","LAT"]],
+                            geometry=gpd.points_from_xy(hubs.LON, hubs.LAT),
+                            crs = "epsg:4326")
+bg_gdf = gpd.read_file(os.path.join(os.getcwd(),"./data/bg_ca_19/blockgroup_CA_19.shp"))
+bg_gdf = bg_gdf.to_crs(hubs_gdf.crs)
+# impute speed on all edges missing data
+graph = ox.add_edge_speeds(graph)
+# calculate travel time (seconds) for all edges
+graph = ox.add_edge_travel_times(graph)
+# %% codecell
+graph_proj = ox.project_graph(graph)
+nodes_proj, edges_proj = ox.graph_to_gdfs(graph_proj, nodes=True, edges=True)
+fig, ax = ox.plot_graph(ox.project_graph(graph))
+# %% codecell
+# Now to only consider hubs and blockgroups within above graph...
+ # (temporary step for debugging just one county before doing all of California)
+min_lon = nodes_proj["lon"].min()
+max_lon = nodes_proj["lon"].max()
+min_lat = nodes_proj["lat"].min()
+max_lat = nodes_proj["lat"].max()
+bb_polygon = shapely.geometry.Polygon([(min_lon,min_lat),
+                      (min_lon,max_lat),
+                      (max_lon,max_lat),
+                      (max_lon,min_lat)])
+hubs_gdf = hubs_gdf[hubs_gdf.within(bb_polygon)]
+bg_gdf = bg_gdf[bg_gdf.centroid.within(bb_polygon)]
+# %% codecell
+
+# distance_matrix = pd.DataFrame(np.nan,index = bg_gdf["GISJOIN"],columns = hubs["id_site"]
+distance_matrix = np.NaN*np.zeros((len(bg_gdf),len(hubs_gdf))) # Initialize array to keep track of distances
+# Calculating distances between hubs and block groups
+for census_lat,census_lon,bg_idx in zip(bg_gdf["INTPTLAT"],bg_gdf["INTPTLON"],range(len(bg_gdf))):
+    # Find all hubs within max_D km
+    # TODO: MODIFY FOLLOWING LOOP TO ONLY DO CLOSEBY HUB
+    for hub_lat,hub_lon,hub_idx in zip(hubs_gdf["LAT"],hubs_gdf["LON"],range(len(hubs_gdf))):
+        distance_matrix[bg_idx,hub_idx] = 5
+
+distance_matrix_df = pd.DataFrame(distance_matrix, index=bg_gdf["GISJOIN"],columns = hubs_gdf["id_site"])
+
+
+
+
+
+
+
+
+
+
+
+
+########### OLD CODE BELOW ###########################
+
+
+#
+# # %% codecell
+# import numpy as np
+# np.sum(edges_proj.isna()["travel_time"])
+# edges_proj
+# # %% codecell
+# from shapely.geometry import box
+# bbox = box(*edges_proj.unary_union.bounds)
+# orig_point = bbox.centroid
+# nodes_proj['x'] = nodes_proj.x.astype(float)
+# maxx = nodes_proj['x'].max()
+# target_loc = nodes_proj.loc[nodes_proj['x']==maxx, :]
+# target_point = target_loc.geometry.values[0]
+# orig_xy = (orig_point.y, orig_point.x)
+# target_xy = (target_point.y, target_point.x)
+# orig_node = ox.get_nearest_node(graph_proj, orig_xy, method='euclidean')
+# target_node = ox.get_nearest_node(graph_proj, target_xy, method='euclidean')
+# o_closest = nodes_proj.loc[orig_node]
+# t_closest = nodes_proj.loc[target_node]
+# od_nodes = gpd.GeoDataFrame([o_closest, t_closest], geometry='geometry', crs=nodes_proj.crs)
+#
+#
+# route = nx.shortest_path(G=graph_proj, source=orig_node, target=target_node, weight='length')
+#
+#
+# fig, ax = ox.plot_graph_route(graph_proj, route)
+#
+# # %% codecell
+# import time
+#
+# t_start = time.process_time()
+#
+# for _ in range(0,10):
+#     route = nx.shortest_path(G=graph_proj, source=orig_node, target=target_node, weight='length')
+#
+# t_end = time.process_time()
+#
+# (t_end-t_start)/10
+#
+# # %% codecell
--- distance_matrix_calculation.py
+++ distance_matrix_calculation.py
@@ -1,121 +1,212 @@
-# %% markdown
-# PROGRESS: File below loads in condidate sites, block group geometries and,
-# graph of Contra Costa county.
-# NEXT STEPS: Write for loop that will.
-# 1. Find all hubs that are within max_D of a given block group
-# 2. Start nested for loop inside to calculate travel time between hubs and blockgroups
-
 # %% codecell
 import osmnx as ox
+from osmnx import utils_graph
 import networkx as nx
+
 import geopandas as gpd
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import os
+from statistics import mean
+
+import matplotlib.pyplot as plt
 import shapely
+import folium
 
-place_name = "Contra Costa County, California, USA"
-max_D = 100
+import os
 
-print('x')
-####### COMMENTED OUT FOLLOWING TWO LINES ARE FOR CHOOSING NEW GRAPH ###########
-# graph = ox.graph_from_place(place_name, network_type='drive')
-# ox.io.save_graphml(graph,os.path.join(os.getcwd(),"./data/",place_name))
-graph = ox.io.load_graphml(os.path.join(os.getcwd(),"./data/",place_name))
-# Load in candidate sites
-hubs = pd.read_csv(os.path.join(os.getcwd(),"./data/candidate_site_campuses_2021-11-17/candidate_site_campuses.csv"))
-hubs = hubs[hubs["cat_site"]!="X"]
-hubs_gdf = gpd.GeoDataFrame(hubs[["id_site","SQFT_ROOF","LON","LAT"]],
-                            geometry=gpd.points_from_xy(hubs.LON, hubs.LAT),
-                            crs = "epsg:4326")
-bg_gdf = gpd.read_file(os.path.join(os.getcwd(),"./data/bg_ca_19/blockgroup_CA_19.shp"))
-bg_gdf = bg_gdf.to_crs(hubs_gdf.crs)
-# impute speed on all edges missing data
+##########################################
+# %% codecell
+location = 'Richmond, California, USA'
+richmond_center = [37.943882, -122.35342]
+
+ca_albers_nad83 = 'NAD_1983_California_Teale_Albers_FtUS'
+nad83 = 'EPSG:4629'
+
+wgs84 = 'EPSG:4326'
+
+##########################################
+# %% codecell
+def plot_gdf_with_background(gdf, zoom=12, center = None):
+
+    if center is not None:
+        map_center = center
+    else:
+        map_center_lon = gdf['geometry'].x.mean()
+        map_center_lat = gdf['geometry'].y.mean()
+        map_center = [map_center_lat, map_center_lon]
+
+    map_with_background = folium.Map(map_center, zoom_start = zoom, width = '50%', height = '50%', tiles = 'OpenStreetMap')
+
+    folium.GeoJson(gdf).add_to(map_with_background)
+
+    return(map_with_background)
+
+
+######### READ IN AND CLEAN NECESSARY DATA
+
+##########################################
+# %% codecell
+import warnings
+
+warnings.filterwarnings('ignore')
+
+graph_raw = ox.graph_from_place(location, network_type = 'drive', simplify = True)
+
+graph = ox.project_graph(graph_raw, to_crs = nad83)
 graph = ox.add_edge_speeds(graph)
-# calculate travel time (seconds) for all edges
 graph = ox.add_edge_travel_times(graph)
+
+graph_nodes_gdf, graph_edges_gdf = ox.graph_to_gdfs(graph, nodes = True, edges = True)
+
+graph_edges_gdf_reset = graph_edges_gdf.reset_index()
+graph_edges_gdf_reset.rename(columns = {'u':'from_node', 'v':'to_node'}, inplace = True)
+
+#ox.plot_graph(graph)
+##########################################
 # %% codecell
-graph_proj = ox.project_graph(graph)
-nodes_proj, edges_proj = ox.graph_to_gdfs(graph_proj, nodes=True, edges=True)
-fig, ax = ox.plot_graph(ox.project_graph(graph))
+hubs_path = os.path.join(os.getcwd(), 'data', 'candidate_site_campuses_2021-11-17', 'candidate_site_campuses.csv')
+
+hubs_df_raw = pd.read_csv(hubs_path)
+hubs_df_raw = hubs_df_raw.loc[hubs_df_raw['cat_site'] != 'X', ['id_site', 'SQFT_ROOF', 'LON', 'LAT']]
+
+hubs_geom = gpd.points_from_xy(hubs_df_raw.LON, hubs_df_raw.LAT, crs = nad83)
+hubs_gdf = gpd.GeoDataFrame(hubs_df_raw, geometry = hubs_geom, crs = nad83)
+
+#plot_gdf_with_background(hubs_gdf, 13, richmond_center)
+
+##########################################
 # %% codecell
-# Now to only consider hubs and blockgroups within above graph...
- # (temporary step for debugging just one county before doing all of California)
-min_lon = nodes_proj["lon"].min()
-max_lon = nodes_proj["lon"].max()
-min_lat = nodes_proj["lat"].min()
-max_lat = nodes_proj["lat"].max()
-bb_polygon = shapely.geometry.Polygon([(min_lon,min_lat),
-                      (min_lon,max_lat),
-                      (max_lon,max_lat),
-                      (max_lon,min_lat)])
-hubs_gdf = hubs_gdf[hubs_gdf.within(bb_polygon)]
-bg_gdf = bg_gdf[bg_gdf.centroid.within(bb_polygon)]
+cengeos_path = os.path.join(os.getcwd(), 'data', 'bg_ca_19', 'blockgroup_CA_19.shp')
+
+cengeos_gdf = gpd.read_file(cengeos_path)
+cengeos_gdf = cengeos_gdf.to_crs(hubs_gdf.crs)
+
+cengeos_lons = [float(intpt) for intpt in cengeos_gdf['INTPTLON']]
+cengeos_lats = [float(intpt) for intpt in cengeos_gdf['INTPTLAT']]
+
+cengeos = pd.DataFrame(cengeos_gdf[['GISJOIN', 'COUNTYFP']])
+cengeos['LON'] = cengeos_lons
+cengeos['LAT'] = cengeos_lats
+
+cengeos_pt_geom = gpd.points_from_xy(x = cengeos.LON,y = cengeos.LAT, crs = nad83)
+cengeos_pt_gdf = gpd.GeoDataFrame(cengeos, geometry = cengeos_pt_geom, crs = nad83)
+
+#plot_gdf_with_background(cengeos_pt_gdf, 13, richmond_center)
+
+##### RESTRICT TO HUBS WITHIN DESIRED AREA
+
+##########################################
 # %% codecell
+lon_max = graph_nodes_gdf['lon'].max()
+lon_min = graph_nodes_gdf['lon'].min()
+lon_avg = (lon_max + lon_min)/2
 
-# distance_matrix = pd.DataFrame(np.nan,index = bg_gdf["GISJOIN"],columns = hubs["id_site"]
-distance_matrix = np.NaN*np.zeros((len(bg_gdf),len(hubs_gdf))) # Initialize array to keep track of distances
-# Calculating distances between hubs and block groups
-for census_lat,census_lon,bg_idx in zip(bg_gdf["INTPTLAT"],bg_gdf["INTPTLON"],range(len(bg_gdf))):
-    # Find all hubs within max_D km
-    # TODO: MODIFY FOLLOWING LOOP TO ONLY DO CLOSEBY HUB
-    for hub_lat,hub_lon,hub_idx in zip(hubs_gdf["LAT"],hubs_gdf["LON"],range(len(hubs_gdf))):
-        distance_matrix[bg_idx,hub_idx] = 5
+lat_max = graph_nodes_gdf['lat'].max()
+lat_min = graph_nodes_gdf['lat'].min()
+lat_avg = (lat_min + lat_max)/2
 
-distance_matrix_df = pd.DataFrame(distance_matrix, index=bg_gdf["GISJOIN"],columns = hubs_gdf["id_site"])
+bbox_coords = [(lon_min, lat_min), (lon_min, lat_max), (lon_max, lat_max), (lon_max, lat_min)]
+bbox_poly = shapely.geometry.Polygon(bbox_coords)
 
+##########################################
+# %% codecell
+hubs_gdf_bbox = hubs_gdf[hubs_gdf.within(bbox_poly)]
 
+#plot_gdf_with_background(hubs_gdf_bbox)
 
+##########################################
+# %% codecell
+cengeos_pt_gdf_bbox = cengeos_pt_gdf[cengeos_pt_gdf.within(bbox_poly)]
 
+#plot_gdf_with_background(cengeos_pt_gdf_bbox, center = richmond_center)
 
+##########################################
+# %% codecell
+cengeos_pt_gdf_bbox_proj = cengeos_pt_gdf_bbox.to_crs(ca_albers_nad83)
 
+cengeos_buffer = cengeos_pt_gdf_bbox_proj.buffer(7920)
 
+cengeos_buffer_gdf_bbox = gpd.GeoDataFrame(cengeos_pt_gdf_bbox['GISJOIN'])
+cengeos_buffer_gdf_bbox['geometry'] = cengeos_buffer
 
+cengeos_buffer_gdf_bbox = cengeos_buffer_gdf_bbox.to_crs(nad83)
 
+#plot_gdf_with_background(cengeos_buffer_gdf_bbox, center = richmond_center)
 
+#### BUILD DISTANCE MATRIX FOR DESIRED AREA
 
+##########################################
+# %% codecell
+n_cengeos = len(cengeos_pt_gdf_bbox)
 
-########### OLD CODE BELOW ###########################
+##########################################
+# %% codecell
+name_index = {i:cengeos_pt_gdf_bbox.iloc[i]['GISJOIN'] for i in range(0, n_cengeos)}
+name_columns = {i:hubs_gdf_bbox.iloc[i]['id_site'] for i in range(0, n_hubs)}
 
+dist_to_hub_matrix = np.NaN*np.zeros((len(cengeos_pt_gdf_bbox), len(hubs_gdf_bbox)))
+dist_to_hub_df = pd.DataFrame(dist_to_hub_matrix)
 
-#
-# # %% codecell
-# import numpy as np
-# np.sum(edges_proj.isna()["travel_time"])
-# edges_proj
-# # %% codecell
-# from shapely.geometry import box
-# bbox = box(*edges_proj.unary_union.bounds)
-# orig_point = bbox.centroid
-# nodes_proj['x'] = nodes_proj.x.astype(float)
-# maxx = nodes_proj['x'].max()
-# target_loc = nodes_proj.loc[nodes_proj['x']==maxx, :]
-# target_point = target_loc.geometry.values[0]
-# orig_xy = (orig_point.y, orig_point.x)
-# target_xy = (target_point.y, target_point.x)
-# orig_node = ox.get_nearest_node(graph_proj, orig_xy, method='euclidean')
-# target_node = ox.get_nearest_node(graph_proj, target_xy, method='euclidean')
-# o_closest = nodes_proj.loc[orig_node]
-# t_closest = nodes_proj.loc[target_node]
-# od_nodes = gpd.GeoDataFrame([o_closest, t_closest], geometry='geometry', crs=nodes_proj.crs)
-#
-#
-# route = nx.shortest_path(G=graph_proj, source=orig_node, target=target_node, weight='length')
-#
-#
-# fig, ax = ox.plot_graph_route(graph_proj, route)
-#
-# # %% codecell
-# import time
-#
-# t_start = time.process_time()
-#
-# for _ in range(0,10):
-#     route = nx.shortest_path(G=graph_proj, source=orig_node, target=target_node, weight='length')
-#
-# t_end = time.process_time()
-#
-# (t_end-t_start)/10
-#
-# # %% codecell
+dist_to_hub_df.rename(index = name_index, columns = name_columns, inplace = True)
+
+##########################################
+# %% codecell
+def get_coords_and_nearest_node(in_row, in_pt_gdf, in_graph=graph):
+
+    pt_coords = [in_pt_gdf.iloc[in_row]['geometry'].y, in_pt_gdf.iloc[in_row]['geometry'].x]
+    pt_nearest_node = ox.get_nearest_node(in_graph, pt_coords, method = 'euclidean')
+
+    return(pt_nearest_node)
+
+def get_nearby_hubs(in_row, in_cengeo_buffers=cengeos_buffer_gdf_bbox, in_hubs=hubs_gdf_bbox):
+
+    cengeo_buffer = in_cengeo_buffers.iloc[in_row]['geometry']
+    cengeo_nearest_hubs = in_hubs[in_hubs.within(cengeo_buffer)]
+
+    return(cengeo_nearest_hubs)
+
+def get_steps_in_route(in_route):
+
+    steps_in_route = []
+
+    for i in range(0, len(in_route)):
+        steps_in_route.append([in_route[i-1], in_route[i]])
+
+    steps_in_route = steps_in_route[1:]
+
+    return(steps_in_route)
+
+def get_travel_time(in_steps, in_edges=graph_edges_gdf_reset):
+
+    travel_times_sec = []
+
+    for step in range(0, len(in_steps)):
+
+        step_in_route = in_steps[step]
+        step_duration_sec = in_edges.loc[(in_edges['from_node'] == step_in_route[0]) & (in_edges['to_node'] == step_in_route[1])]['travel_time']
+        travel_times_sec.append(step_duration_sec)
+
+    travel_times_min = [float(travel_time)/60 for travel_time in travel_times_sec]
+    travel_time_min = round(sum(travel_times_min), 2)
+
+    return(travel_time_min)
+
+##########################################
+# %% codecell
+for cengeo in range(0, n_cengeos):
+
+    id_cengeo = cengeos_pt_gdf_bbox.iloc[cengeo]['GISJOIN']
+
+    node_origin = get_coords_and_nearest_node(cengeo, cengeos_pt_gdf_bbox)
+    hubs_nearby_gdf = get_nearby_hubs(cengeo)
+    n_hubs_nearby = len(hubs_nearby_gdf)
+
+    for hub in range(0, n_hubs_nearby):
+
+        node_target = get_coords_and_nearest_node(hub, hubs_gdf_bbox)
+
+        route_bt_nodes = nx.shortest_path(graph, node_origin, node_target, weight = 'travel_time')
+        travel_times = ox.utils_graph.get_route_edge_attributes(graph, route_bt_nodes, 'travel_time')
+        travel_time = round(sum(travel_times)/60, 2)
+
+        dist_to_hub_df.loc[id_cengeo].iloc[hub] = travel_time
