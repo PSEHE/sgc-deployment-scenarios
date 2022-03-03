f9ccf55b97ed9bbc32f701248a83bc1a07a4f269
audrey-smith <audrey@psehealthyenergy.org>
2022-03-03 11:01:47 -0500

    It actually works now woo

--- data_cleaning.py
+++ data_cleaning.py
@@ -10,61 +10,63 @@ import matplotlib.pyplot as plt
 ###### GENERATE SITE CAPACITY DATA
 # %%codecell
 #https://ccpia.org/occupancy-load-signs/
-hubs_path = os.path.join(os.getcwd(), 'data', 'candidate_site_campuses_2021-11-17', 'candidate_sites_campuses.csv')
+sites_path = os.path.join(os.getcwd(), 'data', 'candidate_site_campuses_2021-11-17', 'candidate_sites_campuses.csv')
 
-hubs_df_raw = pd.read_csv(hubs_path)
-hubs_df = hubs_df_raw.loc[hubs_df_raw['cat_site'] != 'X', ['id_site', 'cat_site', 'SQFT_ROOF', 'LON', 'LAT']]
+sites_df_raw = pd.read_csv(sites_path)
+sites_df = sites_df_raw.loc[sites_df_raw['cat_site'] != 'X', ['id_site', 'cat_site', 'SQFT_ROOF', 'LON', 'LAT']]
 
 occ_limits = pd.DataFrame({'cat_site':['W', 'CC', 'Pri', 'Sec', 'Coll'], 'sqft_pp':[15, 15, 15, 15, 15]})
 #occ_limits = pd.DataFrame({'cat_site':['W', 'CC', 'Pri', 'Sec', 'Coll'], 'sqft_pp':[0.85, 0.85, 0.75, 0.75, 0.75]})
 
-hub_occ_df = pd.merge(hubs_df, occ_limits, on = 'cat_site')
-hub_occ_df['occ_site'] = hub_occ_df['SQFT_ROOF']/hub_occ_df['sqft_pp']
-hub_occ_df = hub_occ_df.loc[:, ['id_site', 'SQFT_ROOF', 'cat_site', 'occ_site']]
+site_occ_df = pd.merge(sites_df, occ_limits, on = 'cat_site')
+site_occ_df['occ_site'] = site_occ_df['SQFT_ROOF']/site_occ_df['sqft_pp']
+site_occ_df = site_occ_df.loc[:, ['id_site', 'SQFT_ROOF', 'cat_site', 'occ_site']]
 
-hub_occ_dict = {}
+site_occ_dict = {}
 
-for i in range(len(hub_occ_df)):
+for i in range(len(site_occ_df)):
 
-    id_site = hub_occ_df.iloc[i].loc['id_site']
-    occ_site = hub_occ_df.iloc[i].loc['occ_site']
+    id_site = site_occ_df.iloc[i].loc['id_site']
+    occ_site = site_occ_df.iloc[i].loc['occ_site']
 
-    hub_occ_dict[id_site] = occ_site
+    site_occ_dict[id_site] = occ_site
 
-hub_sqft_dict = {}
+site_sqft_dict = {}
 
-for i in range(len(hub_occ_df)):
+for i in range(len(site_occ_df)):
 
-    id_site = hub_occ_df.iloc[i].loc['id_site']
-    occ_site = hub_occ_df.iloc[i].loc['SQFT_ROOF']
+    id_site = site_occ_df.iloc[i].loc['id_site']
+    occ_site = site_occ_df.iloc[i].loc['SQFT_ROOF']
 
-    hub_sqft_dict[id_site] = occ_site
+    site_sqft_dict[id_site] = occ_site
 
 ##########################################
 ###### GET POPULATION DATA
 # %%codecell
-cengeo_pop_df = pd.read_csv('data/bg_ca_19/blockgroup_pop_CA_19.csv')
+blockgroup_pop_df = pd.read_csv('data/bg_ca_19/blockgroup_pop_CA_19.csv')
 
-cengeo_pop_dict = {}
+blockgroup_pop_dict = {}
 
-for i in range(len(cengeo_pop_df)):
+for i in range(len(blockgroup_pop_df)):
 
-    cengeo = cengeo_pop_df.iloc[i].loc['GISJOIN']
-    pop = cengeo_pop_df.iloc[i].loc['POP']
+    blockgroup = blockgroup_pop_df.iloc[i].loc['GISJOIN']
+    pop = blockgroup_pop_df.iloc[i].loc['POP']
 
-    cengeo_pop_dict[cengeo] = pop
+    blockgroup_pop_dict[blockgroup] = pop
 
 ##########################################
-###### MAKE DICTIONARY WITH CENGEO, HUB PAIR TUPLE AS KEY
+###### MAKE DICTIONARY WITH blockgroup, site PAIR TUPLE AS KEY
 # %%codecell
-dist_to_hub_df = pd.read_csv('data/distmatrix_contracosta.csv')
+dist_to_site_df = pd.read_csv('data/distmatrix_contracosta.csv')
+dist_to_site_df.set_index('Unnamed: 0', inplace = True)
+dist_to_site_df.index.name = None
 
-dist_to_hub_dict = {}
+dist_to_site_dict = {}
 
-for cengeo in dist_to_hub_df.index:
-    for hub in dist_to_hub_df.columns:
-        if dist_to_hub_df.loc[cengeo, hub] == dist_to_hub_df.loc[cengeo, hub]:
-            dist_to_hub_dict[tuple([cengeo, hub])] = dist_to_hub_df.loc[cengeo, hub]
+for blockgroup in dist_to_site_df.index:
+    for site in dist_to_site_df.columns:
+        if dist_to_site_df.loc[blockgroup, site] == dist_to_site_df.loc[blockgroup, site]:
+            dist_to_site_dict[tuple([blockgroup, site])] = dist_to_site_df.loc[blockgroup, site]
 
 ##########################################
 ###### READ HEAT DAY DATA
@@ -78,9 +80,9 @@ heatdays_df = heatdays_df.loc[:, ['GISJOIN', 'MEAN']].sort_values('MEAN', ascend
 region_df = pd.read_csv('data/candidate_site_campuses_2021-11-17/candidate_sites_campuses.csv')
 region_df = region_df.loc[region_df['cat_site'] != 'X', ['id_site', 'REGION']]
 
-site_region_df = region_df.merge(hub_occ_df, on = 'id_site')
+site_region_df = region_df.merge(site_occ_df, on = 'id_site')
 
-reg_pop = cengeo_pop_df.groupby('REGION').sum()
+reg_pop = blockgroup_pop_df.groupby('REGION').sum()
 reg_site = site_region_df.groupby('REGION').sum().loc[:,'occ_site']
 
 regions = reg_pop.merge(reg_site, on = 'REGION')
@@ -89,7 +91,7 @@ regions['CAP_PP'] = regions['occ_site']/regions['POP']
 ##########################################
 ##### SCALE CES TO BG
 
-# Derived by selecting cols from CES, NRI spatial join csv, which is too large to push to github
+# Derived by selecting cols from CES, NRI spatial join csv, which is too large to push to gitsite
 bg_ces_df = pd.read_csv('data/bg_ca_19/bg19_ces_indicators.csv')
 
 pct_cols = ['PCT_LESSHS', 'PCT_UNEMP', 'PCT_RENT', 'PCT_LINGISO', 'PCT_POV', 'RATE_ASTH', 'RATE_LBW', 'RATE_CVD']
