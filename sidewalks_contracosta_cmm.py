51e50f6b983eec8e93bcc69e18f260886ca4a499
MortonC78483 <75582215+MortonC78483@users.noreply.github.com>
2022-06-24 18:04:33 -0700

    Wilmington shapefile and sidewalks

--- sidewalks_contracosta_cmm.py
+++ sidewalks_contracosta_cmm.py
@@ -1,4 +1,5 @@
-# Plotting various sidewalk filters for Contra Costa County, beginning with only Richmond
+# Plotting various sidewalk filters for Richmond and Wilmington
+
 import osmnx as ox
 from osmnx import utils_graph
 import networkx as nx
@@ -20,6 +21,7 @@ from scipy.sparse import csr_matrix
 
 import warnings
 
+## ------------------------------ RICHMOND -------------------------------------
 # Trying out different filters for sidewalk data
 place = 'Richmond, California'
 
@@ -33,23 +35,27 @@ G_sidewalk = ox.graph_from_place(place, custom_filter=cf_sidewalk)
 print(len(G_orig), 'original')
 print(len(G_walk), 'custom filter')
 print(len(G_sidewalk), 'sidewalk')
-G = nx.compose(G_orig, G_walk)
 
-ax = ox.plot_graph(G_orig, edge_color = 'r')
-ox.plot_graph(G_sidewalk,ax = ax)
+ax = ox.plot_graph(G_orig, edge_color = 'w', node_size = 0)
+ax = ox.plot_graph(G_walk, edge_color = 'w', node_size = 0)
+ax = ox.plot_graph(G_sidewalk, edge_color = 'w', node_size = 0)
 
-nodes = ox.graph_to_gdfs(G_orig, edges=False)
-c = nodes.unary_union.centroid
+## --------------------------- WILMINGTON --------------------------------------
+# Trying out different filters for sidewalk data
+wilmington_path = os.path.join(os.getcwd(), 'data', 'LA_Times_Neighborhood_Boundaries', 'LA_Times_Neighborhood_Boundaries.shp')
+wilmington_gdf = gpd.read_file(wilmington_path)
+shp = wilmington_gdf['geometry'].unary_union
+shp
+cf= ('''["area"!~"yes"]["highway"~"footway|pedestrian|path|steps|living_street|
+        tertiary|residential|service"]["foot"!~"no"]["service"!~"private"]{}''')\
+    .format(ox.settings.default_access)
+cf_sidewalk=('["footway"~"sidewalk"]{}').format(ox.settings.default_access)
+G_orig = ox.graph_from_polygon(shp, network_type = 'walk')
+G_walk = ox.graph_from_polygon(shp, custom_filter=cf)
+# CLAIRE: Not doing sidewalk here b/c for some reason was throwing errors
 
-fig,ax = ox.plot_graph(G_sidewalk, ax=ax, edge_color = 'black')
-plt.show()
+print(len(G_orig), 'original')
+print(len(G_walk), 'custom filter')
 
-fig,ax = plt.subplots(1)
-ox.plot_graph(G_walk,ax=ax,edge_color = 'green')
-
-fig,ax = plt.subplots(1)
-ox.plot_graph(G_orig,ax=ax,edge_color = 'r')
-
-
-# Plot original walking edges in white, custom filter in yellow, sidewalk in green
-# TODO: THIS PLOT
+ax = ox.plot_graph(G_orig, edge_color = 'w', node_size = 0)
+ax = ox.plot_graph(G_walk, edge_color = 'w', node_size = 0)
